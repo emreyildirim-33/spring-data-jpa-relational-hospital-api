6d8a8abd97600458317d4158a9b8d33daa14ed3e
CLADForge <yunusebusiness@gmail.com>
2026-04-04 16:34:04 +0300

    SPRİNG 18 GÜN 3 BİTTİ

--- src/main/java/com/workintech/sqlrelations/entity/Operation.java
+++ src/main/java/com/workintech/sqlrelations/entity/Operation.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.print.Doc;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
@@ -15,9 +17,27 @@ public class Operation {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column(name = "patient_id")
-    private long patientId;
+    @ManyToOne
+    @JoinColumn(name = "doctor_id")
+    private Doctor doctor;
 
-    @Column(name = "doctor_id")
-    private long doctorId;
+    @ManyToOne
+    @JoinColumn(name = "patient_id")
+    private  Patient patient;
+
+    public void setPatientId(long patientId) {
+        Patient p = new Patient();
+        p.setId(patientId);
+        this.patient = p;
+    }
+
+    public void setDoctorId(long doctorId) {
+        Doctor d = new Doctor();
+        d.setId(doctorId);
+        this.doctor = d;
+    }
+
+    public long getDoctorId() {
+        return (this.doctor != null) ? this.doctor.getId(): 0;
+    }
 }
--- src/main/java/com/workintech/sqlrelations/entity/Surgery.java
+++ src/main/java/com/workintech/sqlrelations/entity/Surgery.java
@@ -4,6 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import jakarta.persistence.*;
+import org.springframework.data.repository.query.Param;
+
+import javax.print.Doc;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
@@ -14,12 +18,37 @@ public class Surgery {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column(name = "nurse_id")
-    private long nurseId;
+    @ManyToOne
+    @JoinColumn(name = "nurse_id")
+    private Nurse nurse;
 
-    @Column(name = "patient_id")
-    private long patientId;
+    @ManyToOne
+    @JoinColumn(name = "patient_id")
+    private Patient patient;
 
-    @Column(name = "doctor_id")
-    private long doctorId;
+    @ManyToOne
+    @JoinColumn(name = "doctor_id")
+    private Doctor doctor;
+
+    public void setPatientId(long patientId) {
+        Patient p = new Patient();
+        p.setId(patientId);
+        this.patient = p;
+    }
+
+    public void setNurseId(long nurseId) {
+        Nurse n = new Nurse();
+        n.setId(nurseId);
+        this.nurse = n ;
+    }
+
+    public void setDoctorId(long doctorId) {
+        Doctor d = new Doctor();
+        d.setId(doctorId);
+        this.doctor = d;
+    }
+
+    public long getDoctorId() {
+        return this.doctor.getId();
+    }
 }
